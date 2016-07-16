cfd149533ee0da0cefbd46b2f7800a57de875eb1
Zach <me@zthomae.com>
2016-07-16 11:57:50 -0500

    Finish ch7

--- ch7/roundTrip.hs
+++ ch7/roundTrip.hs
@@ -7,9 +7,9 @@ roundTrip' :: (Show a, Read a) => a -> a
 roundTrip' = read . show
 
 roundTrip2 :: (Show a, Read b) => a -> b
-roundTrip2 a = (read . show) :: String
+roundTrip2 = read . show
 
 main = do
   print (roundTrip 4)
-  print (roundTrip2 4)
+  print (roundTrip2 4 :: Int)
   print (id 4)
