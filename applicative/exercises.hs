6d050d3825dd9bcdbce175c1ceb6299dd9e9f1de
Zach Thomae <me@zthomae.com>
2016-11-25 00:00:08 -0600

    Do applicative chapter exercises

---
+++ applicative/exercises.hs
@@ -0,0 +1,129 @@
+import Control.Applicative
+import Data.Monoid
+import Test.QuickCheck hiding (Failure, Success)
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+-- 1
+
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair a b) = Pair (f a) (f b)
+
+instance Applicative Pair where
+  pure a = Pair a a
+  (Pair f g) <*> (Pair a b) = Pair (f a) (g b)
+
+instance Arbitrary a => Arbitrary (Pair a) where
+  arbitrary = Pair <$> arbitrary <*> arbitrary
+
+instance Eq a => EqProp (Pair a) where
+  (=-=) = eq
+
+-- 2
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+instance Monoid a => Applicative (Two a) where
+  pure x = Two mempty x
+  (Two a f) <*> (Two a' x) = Two (a <> a') (f x)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = Two <$> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b) => EqProp (Two a b) where
+  (=-=) = eq
+
+-- 3
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance (Monoid a, Monoid b) => Applicative (Three a b) where
+  pure x = Three mempty mempty x
+  (Three a b c) <*> (Three a' b' c') = Three (a <> a') (b <> b') (c c')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = Three <$> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+  (=-=) = eq
+
+-- 4
+
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b b') = Three' a (f b) (f b')
+
+instance Monoid a => Applicative (Three' a) where
+  pure x = Three' mempty x x
+  (Three' a f f') <*> (Three' a' b b') = Three' (a <> a') (f b) (f' b')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = Three' <$> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b) => EqProp (Three' a b) where
+  (=-=) = eq
+
+-- 5
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative (Four a b c) where
+  pure x = Four mempty mempty mempty x
+  (Four a b c d) <*> (Four a' b' c' d') =
+    Four (a <> a') (b <> b') (c <> c') (d d')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = Four <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b, Eq c, Eq d) => EqProp (Four a b c d) where
+  (=-=) = eq
+
+-- 6
+
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor (Four' a) where
+  fmap f (Four' a a' a'' b) = Four' a a' a'' (f b)
+
+instance Monoid a => Applicative (Four' a) where
+  pure x = Four' mempty mempty mempty x
+  (Four' a b c f) <*> (Four' x y z d) = Four' (a <> x) (b <> y) (c <> z) (f d)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = Four' <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b) => EqProp (Four' a b) where
+  (=-=) = eq
+
+-- Combinations
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a, b, c)]
+combos = liftA3 (,,)
+
+type SSI = (String, String, Integer)
+main :: IO ()
+main = do
+  quickBatch $ applicative (undefined :: Pair SSI)
+  quickBatch $ applicative (undefined :: Two String SSI)
+  quickBatch $ applicative (undefined :: Three String String SSI)
+  quickBatch $ applicative (undefined :: Three' String SSI)
+  quickBatch $ applicative (undefined :: Four String String String SSI)
+  quickBatch $ applicative (undefined :: Four' String SSI)
