6b34b57914987927242369c3c565871ee5e9c565
Zach Thomae <me@zthomae.com>
2017-02-05 17:42:52 -0600

    One step of morra

--- monad-transformers/morra.hs
+++ monad-transformers/morra.hs
@@ -1,21 +1,55 @@
 module Morra where
 
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
 
 data Score = Score Integer Integer deriving (Eq, Show)
 
-data Guess = Odd | Even deriving (Eq, Show)
+data Player = Odd | Even deriving (Eq, Show)
 
-data Player = One | Two deriving (Eq, Show)
-data Move = Move Player deriving (Eq, Show)
+data Move = One | Two deriving (Eq, Show)
 
-type GameStep = IO (Guess, Move)
+move :: Maybe (Move, Move) -> Score -> (Maybe Player, Score)
+move Nothing s = (Nothing, s)
+move (Just (One, One)) (Score s1 s2) = (Just Even, Score s1 (s2 + 1))
+move (Just (Two, Two)) (Score s1 s2) = (Just Even, Score s1 (s2 + 1))
+move (Just _) (Score s1 s2) = (Just Odd, Score (s1 + 1) s2)
 
-move :: (Guess, Move) -> Score -> Score
-move (guess, Move which) (Score s1 s2)
-  | which == One = Score (s1 + 1) s2
-  | otherwise    = Score s1 (s2 + 1)
-
-advance step = do
+-- Not used...
+advance :: StateT Score IO (Maybe Player)
+advance = do
   s <- get
-  put $ move (Odd, Move One) s
+  (winner, score) <- lift $ step s
+  put score
+  return winner
+
+step :: Score -> IO (Maybe Player, Score)
+step s = do
+  maybeStep <- getMove
+  let m = move maybeStep s
+  putStrLn (show m)
+  return m
+
+moveFromString :: String -> Maybe Move
+moveFromString "one" = Just One
+moveFromString "two" = Just Two
+moveFromString _ = Nothing
+
+getMove :: IO (Maybe (Move, Move))
+getMove = do
+  putStr "First move: "
+  input1 <- getLine
+  putStr "Second move: "
+  input2 <- getLine
+  case (moveFromString input1, moveFromString input2) of
+    (Just m1, Just m2) -> return $ Just (m1, m2)
+    _ -> do
+      putStrLn "Invalid move"
+      return $ Nothing
+
+main :: IO ()
+main = do
+  let initialState = Score 0 0
+  step initialState
+  return ()
