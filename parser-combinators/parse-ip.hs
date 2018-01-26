27990454bcb4e559b7a88bea6ed79c6bc50f6248
Zach Thomae <zach@thomae.co>
2018-01-25 19:56:02 -0600

    Start parsing pieces of ipv6 addresses
    Also address issues/naming with previous ip parsers

--- parser-combinators/parse-ip.hs
+++ parser-combinators/parse-ip.hs
@@ -1,7 +1,7 @@
 module Ipv4Parser where
 
 import Data.Bits ((.&.))
-import Data.Char (digitToInt)
+import Data.Char (chr, digitToInt, ord)
 import Data.List (foldl', intercalate)
 import Data.Maybe (catMaybes)
 import Data.Ord
@@ -23,27 +23,63 @@ instance Show IPAddress where
     in
       intercalate "." [show one, show two, show three, show four]
 
-parseOctet :: Parser Word8
-parseOctet = do
+octet :: Parser Word8
+octet = do
   first <- digit
   rest <- many digit
-  let sum = foldl' (\b a -> b * 10 + a) 0 (digitToInt <$> (first:rest))
-  if sum > 255
-    then fail "octet must be between 0 and 255"
-    else return $ fromIntegral sum
+  if (length (first:rest) > 3)
+    then fail "octet can only be three characters long"
+    else do
+      let sum = foldl' (\b a -> b * 10 + a) 0 (digitToInt <$> (first:rest))
+      if sum > 255
+        then fail "octet must be between 0 and 255"
+        else return $ fromIntegral sum
 
-parseIpv4 :: Parser IPAddress
-parseIpv4 = do
-  one <- parseOctet
+ipv4 :: Parser IPAddress
+ipv4 = do
+  one <- octet
   char '.'
-  two <- parseOctet
+  two <- octet
   char '.'
-  three <- parseOctet
+  three <- octet
   char '.'
-  four <- parseOctet
+  four <- octet
   return (IPAddress $ fromIntegral four + (fromIntegral three * (2^8) + (fromIntegral two * (2^16) + (fromIntegral one * (2^24)))))
 
 data IPAddress6 = IPAddress6 Word64 Word64 deriving (Eq, Ord, Show)
+
+-- TODO: Write my own Show instance
+newtype Hextet = Hextet Word16 deriving (Eq, Ord, Show)
+
+-- this is not very good
+hexToInt :: Char -> Maybe Int
+hexToInt c
+  | c >= '0' && c <= '9' = Just $ ord c - ord '0'
+  | c >= 'a' && c <= 'f' = Just $ ord c - ord 'a' + 10
+  | c >= 'A' && c <= 'F' = Just $ ord c - ord 'A' + 10
+  | otherwise = Nothing
+
+hex :: Parser Int
+hex = do
+  c <- oneOf $ ['0'..'9'] ++ ['a'..'f'] ++ ['A'..'F']
+  case hexToInt c of
+    Just n -> return n
+    Nothing -> fail $ "not a valid hex digit: " ++ [c]
+
+hextet :: Parser Hextet
+hextet = do
+  first <- hex
+  rest <- many hex
+  if length (first:rest) > 4
+    then fail "octet cannot be more than 4 characters long"
+    else do
+      let sum = foldl' (\b a -> b * 16 + a) 0 (first:rest)
+      if sum >= 2^16
+        then fail "a hextet must be between 0 and (2^16)-1"
+        else return (Hextet $ fromIntegral sum)
+
+hextets :: Parser [Hextet]
+hextets = sepBy hextet (char ':')
 
 runTest :: (Eq a, Show a) => Parser a -> String -> Maybe a -> Expectation
 runTest parser input output =
@@ -56,7 +92,7 @@ runTest parser input output =
 main :: IO ()
 main = hspec $ do
   describe "parseIPv4" $ do
-    let test = runTest parseIpv4
+    let test = runTest ipv4
 
     it "should parse example 1" $ test "172.16.254.1" $ Just (IPAddress 2886794753)
     it "should parse example 2" $ test "204.120.0.15" $ Just (IPAddress 3430416399)
@@ -65,3 +101,31 @@ main = hspec $ do
       w <- (arbitrary :: Gen Word32)
       let ip = IPAddress w
       return $ test (show ip) (Just ip)
+
+  describe "hex" $ do
+    let test = runTest hex
+
+    it "should parse example 1" $ test "A" $ Just 10
+    it "should parse example 2" $ test "0" $ Just 0
+    it "should parse example 3" $ test "a" $ Just 10
+
+    it "should not parse example 4" $ test "g" $ Nothing
+
+  describe "hextet" $ do
+    let test = runTest hextet
+
+    it "should parse 0000" $ test "0000" $ Just (Hextet 0)
+    it "should parse 0" $ test "0" $ Just (Hextet 0)
+    it "should parse 000" $ test "000" $ Just (Hextet 0)
+    it "should parse FF" $ test "FF" $ Just (Hextet 255)
+
+    it "should not parse fffff" $ test "fffff" $ Nothing
+    it "should not parse 0000a" $ test "0000a" $ Nothing
+
+  describe "hextets" $ do
+    let test = runTest hextets
+
+    it "should parse 0:0" $ test "0:0" $ Just [Hextet 0, Hextet 0]
+    it "should parse 0:ffff" $ test "0:ffff" $ Just [Hextet 0, Hextet $ fromIntegral (2^16 - 1)]
+
+    it "should not parse 0:g" $ test "0:g" $ Nothing
