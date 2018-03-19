797c91936d59233c8ac44e98122016a37c8e3fc5
Zach Thomae <zach@thomae.co>
2018-03-18 23:09:14 -0500

    Add initial fingerd project

---
+++ fingerd/src/Main.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where
+
+import Control.Exception
+import Control.Monad (forever)
+import Data.List (intersperse)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Data.Typeable
+import Database.SQLite.Simple hiding (close)
+import qualified Database.SQLite.Simple as SQLite
+import Database.SQLite.Simple.Types
+import Network.Socket hiding (close, recv)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Network.Socket.ByteString (recv, sendAll)
+import Text.RawString.QQ
+
+data User =
+  User {
+    userId :: Integer
+  , username :: Text
+  , shell :: Text
+  , homeDirectory :: Text
+  , realName :: Text
+  , phone :: Text
+  } deriving (Eq, Show)
+
+instance FromRow User where
+  fromRow = User <$> field
+                 <*> field
+                 <*> field
+                 <*> field
+                 <*> field
+                 <*> field
+
+instance ToRow User where
+  toRow (User id_ username shell homeDir realName phone) =
+    toRow (id_, username, shell, homeDir, realName, phone)
+
+createUsers :: Query
+createUsers = [r|
+CREATE TABLE IF NOT EXISTS users
+  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+   username TEXT UNIQUE,
+   shell TEXT, homeDirectory TEXT,
+   realName TEXT, phone TEXT)
+|]
+
+insertUser :: Query
+insertUser = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)"
+
+allUsers :: Query
+allUsers = "SELECT * FROM users"
+
+getUserQuery :: Query
+getUserQuery = "SELECT * FROM users WHERE username = ?"
+
+data DuplicateData = DuplicateData deriving (Eq, Show)
+
+instance Exception DuplicateData
+
+type UserRow = (Null, Text, Text, Text, Text, Text)
+
+getUser :: Connection -> Text -> IO (Maybe User)
+getUser conn username = do
+  results <- query conn getUserQuery (Only username)
+  case results of
+    [] -> return Nothing
+    [user] -> return $ Just user
+    _ -> throwIO DuplicateData
+
+createDatabase :: IO ()
+createDatabase = do
+  conn <- open "finger.db"
+  execute_ conn createUsers
+  execute conn insertUser meRow
+  rows <- query_ conn allUsers
+  mapM_ print (rows :: [User])
+  SQLite.close conn
+  where meRow :: UserRow
+        meRow = (Null, "zthomae", "/usr/bin/fish", "/home/zthomae", "Zach Thomae", "555-123-4567")
+
+returnUsers :: Connection -> Socket -> IO ()
+returnUsers dbConn soc = do
+  rows <- query_ dbConn allUsers
+  let usernames = map username rows
+      newlineSeparated = T.concat $ intersperse "\n" usernames
+  sendAll soc (encodeUtf8 newlineSeparated)
+
+formatUser :: User -> ByteString
+formatUser (User _ username shell homeDir realName _) = BS.concat
+  ["Login: ", encodeUtf8 username, "\t\t\t\t",
+   "Name: ", encodeUtf8 realName, "\n",
+   "Directory: ", encodeUtf8 homeDir, "\t\t\t\t",
+   "Shell", encodeUtf8 shell, "\n"]
+
+returnUser :: Connection -> Socket -> Text -> IO ()
+returnUser dbConn soc username = do
+  maybeUser <- getUser dbConn (T.strip username)
+  case maybeUser of
+    Nothing -> do
+      putStrLn ("Couldn't find matching user for username: " ++ (show username))
+      return ()
+    Just user ->
+      sendAll soc (formatUser user)
+
+handleQuery :: Connection -> Socket -> IO ()
+handleQuery dbConn soc = do
+  msg <- recv soc 1024
+  case msg of
+    "\r\n" -> returnUsers dbConn soc
+    name -> returnUser dbConn soc (decodeUtf8 name)
+
+handleQueries :: Connection -> Socket -> IO ()
+handleQueries dbConn sock = forever $ do
+  (soc, _) <- accept sock
+  putStrLn "Got connection, handling query"
+  handleQuery dbConn soc
+  sClose soc
+
+main :: IO ()
+main = withSocketsDo $ do
+  addrinfos <- getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
+                           Nothing
+                           (Just "79")
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+  bindSocket sock (addrAddress serveraddr)
+  listen sock 1
+  -- only one connection open at a time
+  conn <- open "finger.db"
+  handleQueries conn sock
+  SQLite.close conn
+  sClose sock
